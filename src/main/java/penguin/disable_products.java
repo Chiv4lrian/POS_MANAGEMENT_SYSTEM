5a39d7f4cd87ba34fd66986764b5bb15417dd640
Chiv4lrian <hanzostricker24@gmail.com>
2023-12-29 18:21:04 +0800

    Changes

---
+++ src/main/java/penguin/disable_products.java
@@ -0,0 +1,115 @@
+package penguin;
+
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class disable_products {
+    private final StringProperty productId;
+    private final StringProperty name;
+    private final StringProperty category;
+    private final DoubleProperty originalPrice;
+    private final DoubleProperty price;
+    private final IntegerProperty quantity;
+    private final IntegerProperty stockLeft;
+    private final ObjectProperty<LocalDate> dateAssessed;
+    private final ObjectProperty<LocalDate> expiryDate;
+
+    public disable_products(String productId, String name, String category, double originalPrice, double price, int quantity, int stockLeft, LocalDate dateAssessed, LocalDate expiryDate) {
+        this.productId = new SimpleStringProperty(productId);
+        this.name = new SimpleStringProperty(name);
+        this.category = new SimpleStringProperty(category);
+        this.originalPrice = new SimpleDoubleProperty(originalPrice);
+        this.price = new SimpleDoubleProperty(price);
+        this.quantity = new SimpleIntegerProperty(quantity);
+        this.stockLeft = new SimpleIntegerProperty(stockLeft);
+        this.dateAssessed = new SimpleObjectProperty<>(dateAssessed);
+        this.expiryDate = new SimpleObjectProperty<>(expiryDate);
+    }
+
+    public static ObservableList<disable_products> getDisabledProducts(LocalDate date1, LocalDate date2) {
+        ObservableList<disable_products> productList = FXCollections.observableArrayList();
+
+        DBConnect connect = new DBConnect();
+        Connection connection = connect.getConnection();
+
+        if (connection != null) {
+            String query = "SELECT product_id, product_name, category, original_price, sell_price, stock, stock_left, date_added, expire_date FROM product WHERE date_added BETWEEN ? AND ? AND is_disabled = 1";
+
+            try {
+                PreparedStatement statement = connection.prepareStatement(query);
+                statement.setDate(1, java.sql.Date.valueOf(date1));
+                statement.setDate(2, java.sql.Date.valueOf(date2));
+
+                ResultSet resultSet = statement.executeQuery();
+
+                while (resultSet.next()) {
+                    String productId = resultSet.getString("product_id");
+                    String name = resultSet.getString("product_name");
+                    String category = resultSet.getString("category");
+                    double originalPrice = resultSet.getDouble("original_price");
+                    double price = resultSet.getDouble("sell_price");
+                    int quantity = resultSet.getInt("stock");
+                    int stockLeft = resultSet.getInt("stock_left");
+                    LocalDate dateAssessed = resultSet.getDate("date_added").toLocalDate();
+                    LocalDate expiryDate = resultSet.getDate("expire_date").toLocalDate();
+                    productList.add(new disable_products(productId, name, category, originalPrice, price, quantity, stockLeft, dateAssessed, expiryDate));
+                }
+                resultSet.close();
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("Failed to establish a database connection.");
+        }
+
+        return productList;
+    }
+
+    public static ObservableList<disable_products> getDisabledProducts() {
+        return getDisabledProducts(MainController.getSelectedDate3(), MainController.getSelectedDate4());
+    }
+
+    public StringProperty productIdProperty() {
+        return productId;
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public StringProperty categoryProperty() {
+        return category;
+    }
+
+    public DoubleProperty originalPriceProperty() {
+        return originalPrice;
+    }
+
+    public DoubleProperty priceProperty() {
+        return price;
+    }
+
+    public IntegerProperty quantityProperty() {
+        return quantity;
+    }
+
+    public IntegerProperty stockLeftProperty() {
+        return stockLeft;
+    }
+
+    public ObjectProperty<LocalDate> dateAssessedProperty() {
+        return dateAssessed;
+    }
+
+    public ObjectProperty<LocalDate> expiryDateProperty() {
+        return expiryDate;
+    }
+}
---
+++ src/main/java/penguin/sales.java
@@ -0,0 +1,101 @@
+package penguin;
+
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class sales {
+    private final IntegerProperty saleId;
+    private final IntegerProperty productId;
+    private final StringProperty productName;
+    private final DoubleProperty sellPrice;
+    private final IntegerProperty productQuantity;
+    private final DoubleProperty total;
+    private final ObjectProperty<LocalDate> dateAssessed;
+
+    public sales(int saleId, int productId, String productName, double sellPrice, int productQuantity, double total, LocalDate dateAssessed) {
+        this.saleId = new SimpleIntegerProperty(saleId);
+        this.productId = new SimpleIntegerProperty(productId);
+        this.productName = new SimpleStringProperty(productName);
+        this.sellPrice = new SimpleDoubleProperty(sellPrice);
+        this.productQuantity = new SimpleIntegerProperty(productQuantity);
+        this.total = new SimpleDoubleProperty(total);
+        this.dateAssessed = new SimpleObjectProperty<>(dateAssessed);
+    }
+
+    public static ObservableList<sales> getSales(LocalDate date1, LocalDate date2) {
+        ObservableList<sales> salesList = FXCollections.observableArrayList();
+
+        DBConnect connect = new DBConnect();
+        Connection connection = connect.getConnection();
+
+        if (connection != null) {
+            String query = "SELECT sale_id, product_id, product_name, sell_price, product_quantity, total, date_assessed FROM sale WHERE date_assessed BETWEEN ? AND ?";
+
+            try {
+                PreparedStatement statement = connection.prepareStatement(query);
+                statement.setDate(1, java.sql.Date.valueOf(date1));
+                statement.setDate(2, java.sql.Date.valueOf(date2));
+
+                ResultSet resultSet = statement.executeQuery();
+
+                while (resultSet.next()) {
+                    int saleId = resultSet.getInt("sale_id");
+                    int productId = resultSet.getInt("product_id");
+                    String productName = resultSet.getString("product_name");
+                    double sellPrice = resultSet.getDouble("sell_price");
+                    int productQuantity = resultSet.getInt("product_quantity");
+                    double total = resultSet.getDouble("total");
+                    LocalDate dateAssessed = resultSet.getDate("date_assessed").toLocalDate();
+                    salesList.add(new sales(saleId, productId, productName, sellPrice, productQuantity, total, dateAssessed));
+                }
+                resultSet.close();
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("Failed to establish a database connection.");
+        }
+
+        return salesList;
+    }
+
+    public static ObservableList<sales> getSales() {
+        return getSales(MainController.getSelectedDate5(), MainController.getSelectedDate6());
+    }
+
+    public IntegerProperty saleIdProperty() {
+        return saleId;
+    }
+
+    public IntegerProperty productIdProperty() {
+        return productId;
+    }
+
+    public StringProperty productNameProperty() {
+        return productName;
+    }
+
+    public DoubleProperty sellPriceProperty() {
+        return sellPrice;
+    }
+
+    public IntegerProperty productQuantityProperty() {
+        return productQuantity;
+    }
+
+    public DoubleProperty totalProperty() {
+        return total;
+    }
+
+    public ObjectProperty<LocalDate> dateAssessedProperty() {
+        return dateAssessed;
+    }
+}
